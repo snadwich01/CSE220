04b270be46e52eee181af821c1681dfe072a3fd0
snadwich <79296594+snadwich01@users.noreply.github.com>
2025-10-20 16:30:34 +0600

    Create LabTask3.java

---
+++ Lab assignments/class/week 1/LabTask3.java
@@ -0,0 +1,52 @@
+//Lab Task 03: Decryption Process
+class LabTask3{
+
+    //Complete this method so that it gives the Expected Output
+    public static Integer[] decryptMatrix( Integer[][] matrix ){
+
+        //For this task you'll need to create new arrays
+        //we recommend you to use Integer type.
+        //example:  Integer[] array = new Integer[5]
+
+        //TO DO
+        //DELETE the following return statement when you're ready to return the 2D array
+
+        int col_no = matrix[0].length;
+        int[] res = new int[col_no];
+
+        for(int column = 0; column < col_no; column++) {
+            int sum = 0;
+            for(int row = 0; row < matrix.length; row++) {
+                sum += matrix[row][column];
+            }
+            res[column] = sum;
+        }
+
+        Integer[] newarr = new Integer[col_no - 1];
+
+        for(int i = 0; i < newarr.length; i++) {
+            newarr[i] = res[i+1] - res[i];
+        }
+
+        return newarr;
+
+    }
+
+    //DO NOT CHANGE ANY DRIVER CODE BELOW THIS LINE
+    public static void main(String[] args){
+        Integer[][] matrix = {
+            {1,3,1},
+            {6,4,2},
+            {5,1,7},
+            {9,3,3},
+            {8,5,4}
+        };
+        System.out.println("Given Matrix: ");
+        Arr.print2D(matrix);
+        System.out.println("\nExpected Output:\n[ -13 1 ]");
+        Integer[] returned_val_1 = decryptMatrix( matrix );
+        System.out.print("\nYour Output:\n");
+        Arr.print(returned_val_1);
+
+    }
+}
