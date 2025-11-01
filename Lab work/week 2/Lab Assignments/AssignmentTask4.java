a259b48aae2106fa88d9a9ea2ed0a27084a31f8b
snadwich <79296594+snadwich01@users.noreply.github.com>
2025-11-01 23:30:01 +0600

    Create AssignmentTask4.java

---
+++ Lab work/week 2/Lab Assignments/AssignmentTask4.java
@@ -0,0 +1,81 @@
+// AssignmentTask4: ID Generator
+public class AssignmentTask4{
+    
+    // MUST SUBMIT this method
+    public static Node idGenerator(Node head1, Node head2, Node head3) {
+
+        Node curr1 = head1, prev1 = null, next1;
+        Node curr2 = head2;
+        Node curr3 = head3;
+
+        while(curr1 != null) {
+            next1 = curr1.next;
+            curr1.next = prev1;
+            prev1 = curr1;
+            curr1 = next1;
+        }
+
+        Node point = prev1;
+        while(point.next != null) {
+            point = point.next;
+        }
+
+        while(curr2 != null && curr3 != null) {
+            int sum = ((int)curr2.elem + (int)curr3.elem) % 10;
+
+            Node add = new Node(sum);
+
+            point.next = add;
+            point = add;
+
+            curr2 = curr2.next;
+            curr3 = curr3.next;
+        }
+
+        
+        return prev1;
+    }
+
+    //NOTE: if you find any issue with the driver code please inform AIB
+    //DO NOT MAKE ANY MODIFICATIONS IN THE TESTER CODE BELOW
+    //DO NOT SUBMIT THE DRIVER CODE BELOW
+    public static void main(String[] args) {
+        System.out.println("=========Test Case 1=============");
+        Node head1 = LinkedList.createList(new Integer[]{0, 3, 2, 2});
+        Node head2 = LinkedList.createList(new Integer[]{5, 2, 2, 1});
+        Node head3 = LinkedList.createList(new Integer[]{4, 3, 2, 1});
+
+        System.out.print("LinkedList#1:  ");
+        LinkedList.printLL(head1); // This should print 0 -> 3 -> 2 -> 2
+
+        System.out.print("LinkedList#2:  ");
+        LinkedList.printLL(head2); // This should print 5 -> 2 -> 2 -> 1
+
+        System.out.print("LinkedList#3:  ");
+        LinkedList.printLL(head3); // This should print 4 -> 3 -> 2 -> 1
+
+        Node result = idGenerator(head1, head2, head3);
+
+        System.out.print("\nNew ID:  ");
+        LinkedList.printLL(result); // This should print 2 -> 2 -> 3 -> 0 -> 9 -> 5 -> 4 -> 2
+
+        System.out.println("\n=========Test Case 2=============");
+        Node head4 = LinkedList.createList(new Integer[]{0, 3, 9, 1});
+        Node head5 = LinkedList.createList(new Integer[]{3, 6, 5, 7});
+        Node head6 = LinkedList.createList(new Integer[]{2, 4, 3, 8});
+
+        System.out.print("LinkedList#4:  ");
+        LinkedList.printLL(head4); // This should print 0 -> 3 -> 9 -> 1
+
+        System.out.print("LinkedList#5:  ");
+        LinkedList.printLL(head5); // This should print 3 -> 6 -> 5 -> 7
+
+        System.out.print("LinkedList#6:  ");
+        LinkedList.printLL(head6); // This should print 2 -> 4 -> 3 -> 8
+
+        Node result2 = idGenerator(head4, head5, head6);
+
+        System.out.print("\nNew ID:  ");
+        LinkedList.printLL(result2); // This should print 1 -> 9 -> 3 -> 0 -> 5 -> 0 -> 8 -> 5
+    }
+}
