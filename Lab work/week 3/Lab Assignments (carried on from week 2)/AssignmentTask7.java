8d4b9045acd3f884aea8f96bffdb31de37857ab6
snadwich <79296594+snadwich01@users.noreply.github.com>
2025-11-09 23:41:22 +0600

    Create AssignmentTask7.java

---
+++ Lab work/week 3/Lab Assignments (carried on from week 2)/AssignmentTask7.java
@@ -0,0 +1,60 @@
+public class AssignmentTask6 {
+
+    //SUBMIT ONLY THIS METHOD
+    public static void pairJoin(DNode dh1, DNode dh2) {
+        // TO DO
+
+        DNode c1 = dh1.next;
+        DNode c2 = dh2.next;
+        DNode t1 = dh1;
+
+        while(c1 != null && c2 != null) {
+            t1.next = c1;
+            c1.prev = t1;
+            t1 = c1;
+            c1 = c1.next;
+
+            t1.next = c2;
+            c2.prev = t1;
+            t1 = c2;
+            c2 = c2.next;
+        }
+        
+        t1.next = dh1;
+        dh1.prev = t1;
+
+        while(c1 != null) {
+            System.out.print(c1.elem + " ");
+
+            c1 = c1.next;
+        }
+
+    }
+
+    //DO NOT SUBMIT THE DRIVER CODE BELOW
+    //SUBMITTING IT WILL INCREASE YOUR PLAG % FOR NO REASON
+    public static void main(String[] args) {
+        Object[] list1 = {"A", "B", "C", "D"};
+        Object[] list2 = {"E", "F", "G", "H"};
+
+        DNode dh1 = LinkedListHelpers.createDummyHeadedDoublyLL(list1, false);
+        DNode dh2 = LinkedListHelpers.createDummyHeadedDoublyLL(list2, false);
+
+        System.out.println("Given Linked List 1:");
+        LinkedListHelpers.printDummyHeadedDoublyLL(dh1);
+
+        System.out.println("Given Linked List 2:");
+        LinkedListHelpers.printDummyHeadedDoublyLL(dh2);
+
+        System.out.println("\nExpected Output:");
+        Object[] expected = {"A", "E", "B", "F", "C", "G", "D", "H"};
+        DNode expectedHead = LinkedListHelpers.createDummyHeadedDoublyLL(expected, true);
+        LinkedListHelpers.printDummyHeadedDoublyCircularLL(expectedHead);
+
+        //Running the Pair Join
+        pairJoin(dh1, dh2);
+        //Printing after Pair Join
+        System.out.println("\nYour Output:");
+        LinkedListHelpers.printDummyHeadedDoublyCircularLL(dh1);
+    }
+}
