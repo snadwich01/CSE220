64b8a0c1274a26e255e45aad2e11f310e51c3139
snadwich <79296594+snadwich01@users.noreply.github.com>
2025-12-15 00:38:37 +0600

    Add lowestCommonAncestor method in Task1 class
    Implement the lowestCommonAncestor method to find the LCA of two integers in a binary search tree.

---
+++ Lab work/week 6/Lab Task/Task 1.java
@@ -0,0 +1,24 @@
+//Before starting to work on this design the Tree in the Tester code 
+// Complete the lowestCommonAncestor method
+public class Task1 {
+
+    //======================TASK#1======================
+    // This method takes only 3 parameter first one is root
+    // and second & third parameter are as 2 integers
+    // You'll need to find the lowest common ancestor of them and return it
+    public static Integer lowestCommonAncestor( BSTNode root, Integer x, Integer y ){
+        if(root == null) { return null; }
+
+        if (x < root.elem && y < root.elem) {
+        return lowestCommonAncestor(root.left, x, y);
+        }
+        
+        if (x > root.elem && y > root.elem) {
+            return lowestCommonAncestor(root.right, x, y);
+        }
+        
+        return root.elem;
+    }
+    //==================================================
+
+}
