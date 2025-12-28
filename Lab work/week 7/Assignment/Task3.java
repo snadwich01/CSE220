301493b5667d1bdf96e372e22e52869956b9937b
snadwich <79296594+snadwich01@users.noreply.github.com>
2025-12-28 21:30:16 +0600

    Update Task3.java

--- Lab work/week 7/Assignment/Task3.java
+++ Lab work/week 7/Assignment/Task3.java
@@ -1,10 +1,5 @@
+public class Task3 {
 
-import java.util.PriorityQueue;
-
-
-public class Task3{
-
-    //tester
     public static void main(String[] args) {
         int[] tasks = {2, 4, 7, 1, 6};
         int m = 4;
@@ -12,38 +7,138 @@ public class Task3{
         int[] load = sched(tasks, m);
 
         System.out.print("[");
-        for(int i = 0; i < load.length; i++) {
+        for (int i = 0; i < load.length; i++) {
             System.out.print(load[i]);
-            if(i < load.length - 1){
+            if (i < load.length - 1) {
                 System.out.print(", ");
             }
         }
         System.out.println("]");
     }
 
-    //func
     public static int[] sched(int[] tasks, int m) {
-        PriorityQueue<Integer> minheap = new PriorityQueue<>();
+        minheap minheap = new minheap(m);
 
-        for(int i = 0; i < m; i++) {
-            minheap.add(0);
+        for (int i = 0; i < m; i++) {
+            minheap.insert(0);
         }
 
-        for(int i = 0; i < tasks.length; i++) {
+        for (int i = 0; i < tasks.length; i++) {
             int task = tasks[i];
 
-            int head = minheap.poll();
-            int update = task + head;
+            int head = minheap.extractMin();
+            int update = head + task;
 
-            minheap.add(update);
+            minheap.insert(update);
         }
 
-        //type diff
         int[] res = new int[m];
-        for(int i = 0; i < m; i++){
-            res[i] = minheap.poll();
+        for (int i = 0; i < m; i++) {
+            res[i] = minheap.extractMin();
         }
 
         return res;
+    }
+}
+
+    class minheap {
+
+    private int[] heap;
+    private int size;
+
+    public minheap(int capacity) {
+        heap = new int[capacity];
+        size = 0;
+    }
+
+    public void insert(int value) {
+        if (size == heap.length) {
+            return;
+        }
+
+        heap[size] = value;
+        swim(size);
+        size++;
+    }
+
+    private void swim(int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+
+            if (heap[index] >= heap[parent]) {
+                return;
+            }
+
+            swap(index, parent);
+            index = parent;
+        }
+    }
+
+    public int extractMin() {
+        if (size == 0) {
+            return -1;
+        }
+
+        int min = heap[0];
+        heap[0] = heap[size - 1];
+        size--;
+        sink(0);
+
+        return min;
+    }
+
+    private void sink(int index) {
+        while (true) {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int smallest = index;
+
+            if (left < size && heap[left] < heap[smallest]) {
+                smallest = left;
+            }
+
+            if (right < size && heap[right] < heap[smallest]) {
+                smallest = right;
+            }
+
+            if (smallest == index) {
+                return;
+            }
+
+            swap(index, smallest);
+            index = smallest;
+        }
+    }
+
+    public int[] heapsort() {
+
+        int[] copiedHeap = new int[heap.length];
+        for (int i = 0; i < heap.length; i++) {
+            copiedHeap[i] = heap[i];
+        }
+
+        int originalSize = size;
+
+        int[] sorted = new int[size];
+        for (int i = 0; i < sorted.length; i++) {
+            sorted[i] = extractMin();
+        }
+
+        heap = copiedHeap;
+        size = originalSize;
+
+        for (int i = 0; i < sorted.length / 2; i++) {
+            int temp = sorted[i];
+            sorted[i] = sorted[sorted.length - 1 - i];
+            sorted[sorted.length - 1 - i] = temp;
+        }
+
+        return sorted;
+    }
+
+    private void swap(int i, int j) {
+        int temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
     }
 }
