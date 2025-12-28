06f8d25cbbab9c0b721f8de56251f7b0939981da
snadwich <79296594+snadwich01@users.noreply.github.com>
2025-12-28 21:30:03 +0600

    Update Task4.java

--- Lab work/week 7/Assignment/Task4.java
+++ Lab work/week 7/Assignment/Task4.java
@@ -1,37 +1,108 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
-
 public class Task4 {
-    //tester
     public static void main(String[] args) {
         int[] nums = {4, 10, 2, 8, 6, 7};
         int k = 3;
 
-        int[] desc = desc(nums, k);
+        int[] out = desc(nums, k);
 
         System.out.print("[");
-        for(int i = 0; i < desc.length; i++) {
-            System.out.print(desc[i]);
-            if(i < desc.length - 1){
+        for (int i = 0; i < out.length; i++) {
+            System.out.print(out[i]);
+            if (i < out.length - 1) {
                 System.out.print(", ");
             }
         }
         System.out.println("]");
     }
 
-    //func
     private static int[] desc(int[] nums, int k) {
-        PriorityQueue<Integer> maxheap = new PriorityQueue<>(Collections.reverseOrder());
+        MaxHeap maxheap = new MaxHeap(nums.length);
 
-        for(int i = 0; i < nums.length; i++) {
-            maxheap.add(nums[i]);
+        for (int i = 0; i < nums.length; i++) {
+            maxheap.insert(nums[i]);
         }
 
         int[] desc = new int[k];
-        for(int i = 0; i < k; i++) {
-            desc[i] = maxheap.poll();
+        for (int i = 0; i < k; i++) {
+            desc[i] = maxheap.extractMax();
         }
 
-        return  desc;
+        return desc;
+    }
+}
+
+class MaxHeap {
+
+    private int[] heap;
+    private int size;
+
+    public MaxHeap(int capacity) {
+        heap = new int[capacity];
+        size = 0;
+    }
+
+    public void insert(int value) {
+        if (size == heap.length) {
+            return;
+        }
+
+        heap[size] = value;
+        swim(size);
+        size++;
+    }
+
+    private void swim(int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+
+            if (heap[index] <= heap[parent]) {
+                return;
+            }
+
+            swap(index, parent);
+            index = parent;
+        }
+    }
+
+    public int extractMax() {
+        if (size == 0) {
+            return -1;
+        }
+
+        int max = heap[0];
+        heap[0] = heap[size - 1];
+        size--;
+        sink(0);
+
+        return max;
+    }
+
+    private void sink(int index) {
+        while (true) {
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+            int largest = index;
+
+            if (left < size && heap[left] > heap[largest]) {
+                largest = left;
+            }
+
+            if (right < size && heap[right] > heap[largest]) {
+                largest = right;
+            }
+
+            if (largest == index) {
+                return;
+            }
+
+            swap(index, largest);
+            index = largest;
+        }
+    }
+
+    private void swap(int i, int j) {
+        int temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
     }
 }
