eb63448e06a4e88bf99a455afbab8d90ca1c4dd8
snadwich <79296594+snadwich01@users.noreply.github.com>
2026-01-02 16:37:19 +0600

    Update Task2b.java

--- Lab work/Week 8/Task2b.java
+++ Lab work/Week 8/Task2b.java
@@ -31,36 +31,35 @@ public class Task2b {
             }
         }
 
-        public static void addUndirectedEdge(EdgeNode[] adjList, int a, int b, int w) {
+        public static void addundirect(EdgeNode[] adjList, int a, int b, int w) {
             addEdge(adjList, a, b, w);
             addEdge(adjList, b, a, w);
         }
 
         //Task 2b---------------------------------------------------------------
 
+    public static int maxsum(EdgeNode[] adjList) {
 
-        public static int maxSumVertex(EdgeNode[] adjList) {
+        int vert = 0;
+        int maxSum = -1;
 
-            int bestVertex = 1;
-            int bestSum = -1;
+        for (int i = 0; i < adjList.length; i++) {
 
-            for (int i = 1; i < adjList.length; i++) {
+            int sum = 0;
+            EdgeNode cur = adjList[i];
 
-                int sum = 0;
-                EdgeNode cur = adjList[i];
-
-                while (cur != null) {
-                    sum += cur.weight;
-                    cur = cur.next;
-                }
-
-                if (sum > bestSum) {
-                    bestSum = sum;
-                    bestVertex = i;
-                }
+            while (cur != null) {
+                sum += cur.weight;
+                cur = cur.next;
             }
 
-            return bestVertex;
+            if (sum > maxSum) {
+                maxSum = sum;
+                vert = i;
+            }
+        }
+
+        return vert;
         }
     }
 
@@ -69,21 +68,21 @@ public class Task2b {
         int V = 6;
         EdgeNode[] adjList = new EdgeNode[V + 1];
 
-        GraphUtils.addUndirectedEdge(adjList, 1, 2, 1);
-        GraphUtils.addUndirectedEdge(adjList, 1, 3, 2);
-        GraphUtils.addUndirectedEdge(adjList, 1, 6, 3);
+        GraphUtils.addundirect(adjList, 1, 2, 1);
+        GraphUtils.addundirect(adjList, 1, 3, 2);
+        GraphUtils.addundirect(adjList, 1, 6, 3);
 
-        GraphUtils.addUndirectedEdge(adjList, 2, 4, 1);
-        GraphUtils.addUndirectedEdge(adjList, 2, 5, 3);
+        GraphUtils.addundirect(adjList, 2, 4, 1);
+        GraphUtils.addundirect(adjList, 2, 5, 3);
 
-        GraphUtils.addUndirectedEdge(adjList, 3, 4, 3);
-        GraphUtils.addUndirectedEdge(adjList, 3, 5, 3);
+        GraphUtils.addundirect(adjList, 3, 4, 3);
+        GraphUtils.addundirect(adjList, 3, 5, 3);
 
-        GraphUtils.addUndirectedEdge(adjList, 4, 5, 2);
+        GraphUtils.addundirect(adjList, 4, 5, 2);
 
-        GraphUtils.addUndirectedEdge(adjList, 5, 6, 1);
+        GraphUtils.addundirect(adjList, 5, 6, 1);
 
-        int ans = GraphUtils.maxSumVertex(adjList);
-        System.out.println("Vertex: " + ans);
+        int ans = GraphUtils.maxsum(adjList);
+        System.out.println("Maxsum vertex: " + ans);
     }
 }
